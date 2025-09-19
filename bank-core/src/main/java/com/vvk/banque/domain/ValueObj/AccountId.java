1ec33c648ad63122601d66790dbd9d66d8a3dbdb
Vivek <samant.vvek@gmail.com>
2025-09-19 12:25:06 +0530

    feat: added AccountId.java code & CustomerId value objects, tweaked Money.java

---
+++ bank-core/src/main/java/com/vvk/banque/domain/ValueObj/AccountId.java
@@ -0,0 +1,45 @@
+package com.vvk.banque.domain.ValueObj;
+
+import java.util.Objects;
+
+public final class AccountId {
+	private final int acc;
+
+	public AccountId(int acc) {
+		if (acc <= 0) {
+			throw new IllegalArgumentException("AccountId must be positive");
+		}
+
+		if (String.valueOf(acc).length() != 3) {
+			throw new IllegalArgumentException("invalid length of acc id- must be exactly 3 digits");
+		}
+			
+			this.acc = acc;
+	}
+
+	//getter\
+	
+	public getAcc() {
+		return acc;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this ==o) return true;
+		if (! (o instanceof AccountId)) return false;
+		AccountId that = (AccountId) o;
+		return acc == that.acc;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(acc);
+		}
+
+	@Override
+	public String toString() {
+		return String.valueOf(acc);
+		}
+} 
+		
+ 
