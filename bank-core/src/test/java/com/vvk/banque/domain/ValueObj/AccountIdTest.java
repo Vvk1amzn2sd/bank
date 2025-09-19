13c9c4bcc60ffdafeb335a02072f50857efe06ab
Vivek <samant.vvek@gmail.com>
2025-09-19 13:57:31 +0530

    feat: enforce 5-digit accid w/o leading zero; unit-test green

--- bank-core/src/test/java/com/vvk/banque/domain/ValueObj/AccountIdTest.java
+++ bank-core/src/test/java/com/vvk/banque/domain/ValueObj/AccountIdTest.java
@@ -7,21 +7,7 @@ class AccountIdTest {
 	
 	@Test
 	void acceptsExactly5Digits() {
-		AccountId id = new AccountId(00001);
-		assertThat(id.getAcc()).isEqualTo(12345);
+		AccountId id = new AccountId(10001);
+		assertThat(id.getAcc()).isEqualTo(10001);
 	}
-/*
-	@Test
-	void rejectLT5() {
-		assertThatIllegalArgumentException()
-			.isThrownBy( () -> new AccountId(00000))
-			.withMessageContaining("must be +ve");
-	}
-
-	@Test
-	void rejectMT5() {
-		assertThatIllegalArgumentException()
-			.isThrownBy( () -> new AccountId(12388))
-			.withMessageContaining("too long, exactly 5 allowed?");
-	}	*/
 }
