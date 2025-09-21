e8f687839cc4e4a9e13f3334f357c4c08d533584
Vivek <samant.vvek@gmail.com>
2025-09-22 00:35:55 +0530

    feat:green test for Account agg, event correctly built using FromHistry method.

--- bank-core/src/test/java/com/vvk/banque/domain/AggregatesObj/AccountTest.java
+++ bank-core/src/test/java/com/vvk/banque/domain/AggregatesObj/AccountTest.java
@@ -25,7 +25,7 @@ class AccountTest {
 
 	// mock dbseq for gen test acc no.
 	private final DatabaseSequence mockDbSeq = new DatabaseSequence () {
-		private int acc = 10000;
+		private int acc = 10100;
 		@Override
 		public int nextAcc() {
 			return acc++;
@@ -37,12 +37,15 @@ class AccountTest {
 		accId	 	= AccountId.generateUnique(mockDbSeq);
 		beneficiaryId 	= AccountId.generateUnique(mockDbSeq);
 		ownId		= CustomerId.generate("VVK");
-		initialBal	= Money.of(new BigDecimal("100.0"), Currency.getInstance("USD"));
+		initialBal	= Money.of(new BigDecimal("100.0"), Currency.getInstance("INR"));
 		account		= Account.open(accId, ownId, initialBal);
 		} 
 
 	// -- Account.open() Tests --
-	
+
+/*--- testing static open() factory -> correctly creates a new Account Object when valid non-negative opening bal
+	make sure accId, ownId, initialBal set correctly + single AccountOpened event is recorded in uncommitted event lis----*/	
+
 	@Test
 	void test1_OpenAccount_ValidOpeningBalance_Success() {
 		assertNotNull(account);
@@ -60,9 +63,38 @@ class AccountTest {
 		assertEquals(initialBal, event.getOpenBal());
 		}
 
+// Test2 passing -ve  balance exception- seccond line of defence, first one lives in domain where money vo, monies cnt be -ve
 	@Test
 	void test2_OpenAccount_NegativeOpeningBalance_ThrowsException() {
-		Money negativeBalance = Money.of(new BigDecimal("-50.0"), Currency.getInstance("USD"));
+		Money negativeBalance = Money.of(new BigDecimal("-100.0"), Currency.getInstance("INR"));
 		assertThrows(OpeningBalanceNullException.class, () -> Account.open(accId, ownId, negativeBalance));
 		} 
+		
+
+//Test3 - passing null balance as staring
+	
+	@Test
+	void test3_OpenAccount_NullOpeningBalance_ThrowsException() {
+		assertThrows(OpeningBalanceNullException.class, () -> Account.open(accId, ownId, null));
+		}
+
+//Test4 -- Account.fromHistry() Tests ----
+
+	@Test
+	void testFromHistry_WithAccountOpenedEvent_RebuildsStateCorrectly() {
+		Money demo_start_bal = Money.of(new BigDecimal("100.0"), Currency.getInstance("INR"));
+		List<DomainEvent> history = List.of(new AccountOpened(accId, ownId, demo_start_bal));
+		Account reconstructedAcc  = Account.fromHistry(accId, history);
+		
+		assertNotNull(reconstructedAcc);
+		assertEquals(accId, reconstructedAcc.getAID());
+		assertEquals(ownId, reconstructedAcc.getOwnerId()); 	
+		assertEquals(initialBal, reconstructedAcc.getBalance());
+		assertTrue(reconstructedAcc.getUncommittedEvents().isEmpty());
+		}
 }
+
+
+
+
+	
