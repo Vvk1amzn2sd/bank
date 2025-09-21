a318adc3373bd8fa7bf4dc47ea1a9463735ad325
Vivek <samant.vvek@gmail.com>
2025-09-22 03:16:17 +0530

    packaging domain into jar, removed hardcoded adapter and its test in final jar

--- bank-core/src/main/java/com/vvk/banque/adapter/driving/cli/MainCli.java
+++
@@ -1,7 +0,0 @@
-package com.vvk.banque.adapter.driving.cli;
-
-public class MainCli {
-    public static void main(String[] args) {
- 	       System.out.println("Balance: 50 EUR"); // hard-coded green for CLI adapter skeleton
-    }
-}
